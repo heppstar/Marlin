c4e0a24bf7efb512fc56dc9bb5d0de747ea8acfd
heppstar <tobbeerlandsson@hotmail.com>
2021-10-30 15:10:20 +0200

    Nytt test

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2244,7 +2244,7 @@
   #define INTERPOLATE       false // Interpolate X/Y/Z_MICROSTEPS to 256 //Tobbe
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       1400        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
@@ -2260,7 +2260,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       1400
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
  //   #define Y_RSENSE          0.075 //Tobbe TMC5160
@@ -2277,7 +2277,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT        1400
+    #define Z_CURRENT        800
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2309,21 +2309,21 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      1400
+    #define E0_CURRENT      800
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
   #endif
 
   #if AXIS_IS_TMC(E1)
-    #define E1_CURRENT      1400
+    #define E1_CURRENT      800
     #define E1_MICROSTEPS    16
     #define E1_RSENSE         0.11
     #define E1_CHAIN_POS     -1
   #endif
 
   #if AXIS_IS_TMC(E2)
-    #define E2_CURRENT      1400
+    #define E2_CURRENT      800
     #define E2_MICROSTEPS    16
     #define E2_RSENSE         0.11
     #define E2_CHAIN_POS     -1
@@ -2468,7 +2468,7 @@
    * M912 - Clear stepper driver overtemperature pre-warn condition flag.
    * M122 - Report driver parameters (Requires TMC_DEBUG)
    */
-  #define MONITOR_DRIVER_STATUS //Tobbe
+  //#define MONITOR_DRIVER_STATUS //Tobbe
 
   #if ENABLED(MONITOR_DRIVER_STATUS)
     #define CURRENT_STEP_DOWN     50  // [mA]
@@ -2565,7 +2565,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continous reporting.
    */
-  #define TMC_DEBUG //Tobbe
+  //#define TMC_DEBUG //Tobbe
 
   /**
    * You can set your own advanced settings by filling in predefined functions.
@@ -3538,7 +3538,7 @@
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-#define PINS_DEBUGGING //Tobbe
+//#define PINS_DEBUGGING //Tobbe
 
 // Enable Marlin dev mode which adds some special commands
 //#define MARLIN_DEV_MODE
